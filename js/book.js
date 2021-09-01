6208a0f05e3d274f40189ce8501ce0e8d25da344
mehesultana <sultanamehe01@gmail.com>
2021-09-02 04:00:59 +0600

    create dynamic url

--- js/book.js
+++ js/book.js
@@ -10,5 +10,27 @@ const searchBook = () => {
     //console.log(url);
     fetch(url)
         .then((res) => res.json())
-        .then((data) => console.log(data.docs));
+        .then((data) => displaySearchResult(data.docs));
+};
+
+const displaySearchResult = (docs) => {
+    console.log(docs);
+    const searchResult = document.getElementById('search-result');
+    docs.forEach((doc) => {
+        console.log(doc.author_name[0]);
+        const { title } = doc.title;
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+        <div class="card h-100">
+                        <img src=".." class="card-img-top" alt="..." />
+                        <div class="card-body">
+                            <h3 class="card-title">${doc.title}</h3>
+                            <h6 class="card-title">${doc.author_name[0]}</h6>
+                            
+                        </div>
+                    </div>
+        `;
+        searchResult.appendChild(div);
+    });
 };
