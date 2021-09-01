659742b85fd55286198054e913aa922faca08749
mehesultana <sultanamehe01@gmail.com>
2021-09-02 04:37:06 +0600

    spinner add

--- js/book.js
+++ js/book.js
@@ -1,4 +1,14 @@
+// loading spinner
+const loadingSpinner = document.getElementById('loadingSpinner');
+const toggleSpinner = (show) => {
+    const spinner = loadingSpinner;
+    // console.log(spinner);
+
+    show ? spinner.classList.remove('d-none') : spinner.classList.add('d-none');
+};
+
 const searchBook = () => {
+    toggleSpinner(true);
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
     //console.log(searchText);
@@ -14,6 +24,7 @@ const searchBook = () => {
 };
 
 const displaySearchResult = (docs) => {
+    toggleSpinner(false);
     console.log(docs);
     const searchResult = document.getElementById('search-result');
     docs?.forEach((doc) => {
@@ -23,7 +34,7 @@ const displaySearchResult = (docs) => {
         div.classList.add('col');
         div.innerHTML = `
         <div class="card h-100">
-                        <img src="https://covers.openlibrary.org/b/id/${doc?.cover_i}-M.jpg" class="card-img-top" alt="..." />
+                        <img src="https://covers.openlibrary.org/b/id/${doc?.cover_i}-M.jpg" class="card-img-top " alt="..." />
                         <div class="card-body">
                             <h3 class="card-title">${doc.title.slice(0, 8)}</h3>
                             <h6 class="card-title text-warning">${doc.author_name[0]}</h6>
