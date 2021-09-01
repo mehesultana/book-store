9814f2a9b854744584ca7d9c3fa69dab7adbd1bf
mehesultana <sultanamehe01@gmail.com>
2021-09-02 04:16:19 +0600

    image add

--- js/book.js
+++ js/book.js
@@ -26,7 +26,8 @@ const displaySearchResult = (docs) => {
                         <img src="https://covers.openlibrary.org/b/id/${doc?.cover_i}-M.jpg" class="card-img-top" alt="..." />
                         <div class="card-body">
                             <h3 class="card-title">${doc.title.slice(0, 8)}</h3>
-                            <h6 class="card-title">${doc.author_name[0]}</h6>
+                            <h6 class="card-title text-warning">${doc.author_name[0]}</h6>
+                            <h6 class="card-title">publisher: ${doc.publisher[0]}</h6>
                             
                         </div>
                     </div>
